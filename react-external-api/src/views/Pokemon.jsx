0e3f86cef19f7fb44fcd0e5c50147aad78450dac
José Carneiro <josemcarneiro@gmail.com>
2019-09-26 23:53:16 +0100

    Fixed pokemon state update on route param change

--- react-external-api/src/views/Pokemon.jsx
+++ react-external-api/src/views/Pokemon.jsx
@@ -6,22 +6,21 @@ export default class Pokemon extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: false,
       pokemon: null
     };
   }
 
-  static getDerivedStateFromProps(props, state) {
-    const id = props.match.params.id;
-    return {
-      ...state,
-      id
-    };
-  }
+  // static getDerivedStateFromProps(props, state) {
+  //   const id = props.match.params.id;
+  //   return {
+  //     ...state,
+  //     id
+  //   };
+  // }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.id !== this.state.id || !this.state.pokemon) {
-      const id = this.props.match.params.id;
+    const id = this.props.match.params.id;
+    if (prevProps.match.params.id !== id || !this.state.pokemon) {
       load(id)
         .then(pokemon => {
           this.setState({
